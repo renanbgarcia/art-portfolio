edeb18b9e6236bc9b107c7101565737608613564
Renan Garcia <bg_renan@hotmail.com>
2019-07-30 16:50:25 -0300

    partial

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
-import './App.css';
+import './styles/index.css';
 import Home from './Home';
-import Quiz from './Quiz';
 
 class App extends Component {
   render() {
@@ -10,10 +9,10 @@ class App extends Component {
       <Router>
         <div className="App">
           <header className="App-header">
+            <div className="main-name">Renan</div>
           </header>
           <Switch>
             <Route exact path="/" component={Home} />
-            <Route path="/quiz" component={Quiz} />
           </Switch>
         </div>
       </Router>
--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import Button from '@material-ui/core/Button';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+import image1 from './assets/image1.jpg';
 
 class Home extends Component {
     
     render() {
         return (
             <div>
-                <Button variant="contained" color="primary">
-                    <Link className="App-link" to="/quiz">Quiz</Link>
-                </Button>
+                <Carousel
+                  showThumbs={false}
+                  showStatus={false}
+                  useKeyboardArrows
+                  showIndicators={false}
+                  dynamicHeight={false}
+                  >
+                    <div className="slides">
+                        <img src={image1} />
+                    </div>
+                    <div>
+                        <img src="assets/image2.jpg" />
+                        <p className="legend">Legend 2</p>
+                    </div>
+                </Carousel>
             </div>
         );
     }
